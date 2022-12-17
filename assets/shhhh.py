ef652d04ad740b524b457c799ae153df234230de
Anastacio Ruiz <anastacior21@gmail.com>
2022-12-16 21:41:34 -0800

    fixed dollar sign bug

--- assets/shhhh.py
+++ assets/shhhh.py
@@ -1,2 +1,2 @@
-TOKEN = '' # add token in quotes
-CHANNEL = '' # remove quotes and add the channel id here
+TOKEN = 'MTAzNTA3MTY1ODYwMTUwMDcwMg.G5eDOi.ERdTc4V5jppWRoliLRiQE5VJBL2C1x8r4L1utw' # add token in quotes
+CHANNEL = 1053529689781714975 # remove quotes and add the channel id here
